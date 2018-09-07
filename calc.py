1d0b38bc5f878b5f9f5f7b24bb75fc753cd10d63
kouta Nakanishi <nizi26i26@gmail.com>
2018-09-07 17:19:38 +0900

    input_to_dataのテストと実装

--- calc.py
+++ calc.py
@@ -9,3 +9,16 @@ class Calc:
         if tmp%10>=5:
             result+=1
         return result
+
+    def input_to_data(self,input):
+
+        ret = []
+        for arr in input:
+            ret.append(arr.strip().split(","))
+
+
+        for i in range(len(ret)):
+            for j in range(len(ret[i])):
+                ret[i][j]=int(ret[i][j])
+                
+        return ret
--- test/calc.py
+++ test/calc.py
@@ -1,12 +1,29 @@
 import unittest
+import sys
 from calc import Calc
-
+import io
 class TestCalc(unittest.TestCase):
-    def test_Calc(self):
+
+    def test_io(self):
         calc = Calc()
-        self.assertEqual(calc.calc_price([10, 12]),24)
-        self.assertEqual(calc.calc_price([40, 16]),62)
-        self.assertEqual(calc.calc_price([100, 45]),160)
-        self.assertEqual(calc.calc_price([50, 50, 55]), 171)
-        self.assertEqual(calc.calc_price([]), 0)
-        self.assertEqual(calc.calc_price([11, 12, 13]),40)
+        test_case= "12,13\n11,11\n"
+        input=io.StringIO(test_case)
+        self.assertEqual(calc.input_to_data(input),[[12,13],[11,11]])
+
+        test_case= "12,13,11\n130\n"
+        input=io.StringIO(test_case)
+        self.assertEqual(calc.input_to_data(input),[[12,13,11],[130]])
+
+
+                #output.write("HELLO")
+
+                #sys.stdout.write(z+"AAA")
+
+                #self.assertEqual(calc.calc_price(,24)
+
+
+                #self.assertEqual(calc.calc_price([40, 16]),62)
+                #self.assertEqual(calc.calc_price([100, 45]),160)
+                #self.assertEqual(calc.calc_price([50, 50, 55]), 171)
+                #self.assertEqual(calc.calc_price([]), 0)
+                #self.assertEqual(calc.calc_price([11, 12, 13]),40)
