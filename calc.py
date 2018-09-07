478769f2998bcad4af93686f5612625aa66d1ebf
takahito1220 <b1606966@planet.kanazawa-it.ac.jp>
2018-09-07 18:52:35 +0900

    課題2-2

--- calc.py
+++ calc.py
@@ -1,26 +1,38 @@
-
+import sys
 class Calc:
-    def calc_price(self, a):
-        result = 0
-        for i in a:
-            result += i
-        tmp=result
-        result+=tmp//10
-        if tmp%10>=5:
-            result+=1
-        return result
+    def calc_price(self, b, output):
+
+        for a in b:
+
+            result = 0
+            for i in a:
+                result += i
+            tmp=result
+            result+=tmp//10
+            if tmp%10>=5:
+                result+=1
+            output.write(str(result)+ "\n")
 
 
     def input_to_data(self,input):
 
         ret = []
-        for arr in input:
-            ret.append(arr.strip().split(","))
 
+        for arr in input:
+            if len(arr.strip())>0:
+                ret.append(arr.strip().split(","))
+            else:
+                ret.append([0])
+
+        #if len(ret)==0:
+        #    return []
 
         for i in range(len(ret)):
             for j in range(len(ret[i])):
                 ret[i][j]=int(ret[i][j])
-                
+
         return ret
 
+if __name__ == '__main__':
+    calc=Calc()
+    calc.calc_price(calc.input_to_data(sys.stdin),sys.stdout)
--- test/calc.py
+++ test/calc.py
@@ -15,13 +15,39 @@ class TestCalc(unittest.TestCase):
         input=io.StringIO(test_case)
         self.assertEqual(calc.input_to_data(input),[[12,13,11],[130]])
 
+    def test_calc(self):
+        calc = Calc()
+
+        test_case = "\n10,12\n40,16\n100,45\n50,50,55\n\n"
+        input = io.StringIO(test_case)
+        output =io.StringIO()
+        calc.calc_price(calc.input_to_data(input),output)
+
+        self.assertEqual(output.getvalue(), "0\n24\n62\n160\n171\n0\n")
+        sys.stdout.write(output.read())
+        '''
+        test_case = "40,16\n"
+        input = io.StringIO(test_case)
+        self.assertEqual(calc.calc_price(input), 62)
+
+        test_case = "100,45\n"
+        input = io.StringIO(test_case)
+        self.assertEqual(calc.calc_price(input), 160)
+
+        test_case = "50,50,55\n"
+        input = io.StringIO(test_case)
+        self.assertEqual(calc.calc_price(input), 171)
+
+        test_case = "\n"
+        input = io.StringIO(test_case)
+        self.assertEqual(calc.calc_price(input), 0)
+        '''
 
                 #output.write("HELLO")
 
                 #sys.stdout.write(z+"AAA")
 
                 #self.assertEqual(calc.calc_price(,24)
-
 
                 #self.assertEqual(calc.calc_price([40, 16]),62)
                 #self.assertEqual(calc.calc_price([100, 45]),160)
